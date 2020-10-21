1dd31ad123e6dd8987b6c89f10917d8de703f320
MokkoFm <mokkofmpoetry@gmail.com>
2020-10-21 12:22:23 +0300

    add static

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -5,8 +5,8 @@ env = Env()
 env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -114,3 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
